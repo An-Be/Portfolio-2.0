aea61d9ca9bbda5c9de0497e3af21a46f5d585d2
Andrea <andrea.berrocal95@gmail.com>
2022-06-13 18:13:27 -0700

    added typed.js for type writer effect

---
+++ script.js
@@ -0,0 +1,8 @@
+
+//using typed.js
+let typed = new Typed(".header-text", {
+    strings: ["Junior Software Engineer.", "gamer.", "food lover."],
+    typeSpeed: 200,
+    backSpeed: 200,
+    loop: true
+});
