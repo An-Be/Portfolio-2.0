f40a3f899a3cc3b0ca3f45c7decf15482b391d1c
Andrea <andrea.berrocal95@gmail.com>
2022-06-13 18:38:41 -0700

    🎨 formatted code with prettier

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-
 //using typed.js
 let typed = new Typed(".header-text", {
-    strings: ["Junior Software Engineer.", "gamer.", "food lover."],
-    typeSpeed: 200,
-    backSpeed: 200,
-    loop: true
+  strings: ["Junior Software Engineer.", "gamer.", "food lover."],
+  typeSpeed: 200,
+  backSpeed: 200,
+  loop: true,
 });
