c9500eef9f5c22d64feff7118b369c250929f616
Andrea <andrea.berrocal95@gmail.com>
2022-06-15 08:41:54 -0700

    formatted code and removed commented out form to add that in another branch instead

--- script.js
+++ script.js
@@ -5,23 +5,3 @@ let typed = new Typed(".header-text", {
   backSpeed: 200,
   loop: true,
 });
-
-
-//function to get form data and make an alert
-
-//get required elements
-
-/*
-const fname = document.getElementById('fname');
-const lname = document.getElementById('lname');
-const email = document.getElementById('email');
-const message = document.getElementById('message');
-const button = document.getElementById('submit');
-
-function submitForm(){
-  alert(`Thank you for submitted your message, a server is not connected so it was not actually sent but you submitted the following info:\nFirst Name: ${fname.value}\nLast Name: ${lname.value}\nEmail: ${email.value}\nMessage: ${message.value}`);
-}
-
-//event handler
-button.addEventListener('click', submitForm);
-*/
